33027a30a72d80a8a33e2c2e6d85a124ee0ed889
yunjoo <yunjoo.jenn@gmail.com>
2024-08-19 22:04:23 +0900

    No.10828

---
+++ test.py
@@ -0,0 +1,32 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) != 0:
+            print(stack[-1])
+            stack.pop(-1)
+        else:
+            print('-1')
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0 :
+            print('1')
+        else:
+            print(len(stack))
+    else:
+        if len(stack) == 0:
+            print('-1')
+        else:
+            print(stack[-1])
+
+
+
+
